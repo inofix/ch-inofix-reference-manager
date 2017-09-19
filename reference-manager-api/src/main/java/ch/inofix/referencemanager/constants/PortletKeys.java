71a634b2aa11434236e7ec7d284bfde56d6aebb6
Christian Berndt <berndt@inofix.ch>
2017-09-19 15:01:49 +0200

    Fixed REFERENCE_EDITOR key.

--- reference-manager-api/src/main/java/ch/inofix/referencemanager/constants/PortletKeys.java
+++ reference-manager-api/src/main/java/ch/inofix/referencemanager/constants/PortletKeys.java
@@ -4,15 +4,15 @@ package ch.inofix.referencemanager.constants;
  * 
  * @author Christian Berndt
  * @created 2016-11-18 21:56
- * @modified 2017-02-09 19:40
- * @version 1.0.6
+ * @modified 2017-09-19 13:30
+ * @version 1.0.7
  * 
  */
 public class PortletKeys {
 
     public static final String BIBLIOGRAPHY_MANAGER = "ch_inofix_referencemanager_web_internal_portlet_BibliographyManagerPortlet";
     public static final String POPULAR_BIBLIOGRRAPHIES = "ch_inofix_referencemanager_web_internal_portlet_PopularBibliographiesPortlet";
-    public static final String REFERENCE_EDITOR = "referencemanagerjsf_WAR_referencemanagerjsf";
+    public static final String REFERENCE_EDITOR = "referencemanagerjsf_WAR_chinofixreferencemanagerjsf";
     public static final String REFERENCE_MANAGER = "ch_inofix_referencemanager_web_internal_portlet_ReferenceManagerPortlet";
     public static final String YOUR_BIBLIOGRAPHIES = "ch_inofix_referencemanager_web_internal_portlet_YourBibliographiesPortlet";
 }
