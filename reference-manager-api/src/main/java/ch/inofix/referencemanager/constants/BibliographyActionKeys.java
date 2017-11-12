ddb4398357a2b4d5511f64d524aedf444c3d3fa2
Christian Berndt <berndt@inofix.ch>
2017-11-12 21:35:19 +0100

    New deleteGroupBibliographies method.

--- reference-manager-api/src/main/java/ch/inofix/referencemanager/constants/BibliographyActionKeys.java
+++ reference-manager-api/src/main/java/ch/inofix/referencemanager/constants/BibliographyActionKeys.java
@@ -4,13 +4,14 @@ package ch.inofix.referencemanager.constants;
  * 
  * @author Christian Berndt
  * @created 2016-11-29 21:57
- * @modified 2016-11-29 21:57
- * @version 1.0.0
+ * @modified 2017-11-12 21:24
+ * @version 1.0.1
  *
  */
 public class BibliographyActionKeys extends com.liferay.portal.kernel.security.permission.ActionKeys {
 
     public static final String ADD_BIBLIOGRAPHY = "ADD_BIBLIOGRAPHY";
+    public static final String DELETE_GROUP_BIBLIOGRAPHIES = "DELETE_GROUP_BIBLIOGRAPHIES";
     public static final String IMPORT_BIBLIOGRAPHY = "IMPORT_BIBLIOGRAPHY";
 
 }
