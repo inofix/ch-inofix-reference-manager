197b524d97462d602b3e9dcf2e800445c5e2f8fe
Christian Berndt <berndt@inofix.ch>
2017-11-13 13:38:44 +0100

    Add reference-manager and bibliography manager to admin menu.

---
+++ reference-manager-web/src/main/java/ch/inofix/referencemanager/web/internal/application/BibliographyManagerPanelApp.java
@@ -0,0 +1,42 @@
+package ch.inofix.referencemanager.web.internal.application;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.model.Portlet;
+
+import ch.inofix.referencemanager.constants.PortletKeys;
+
+/**
+ *
+ * @author Christian Berndt
+ * @created 2017-11-13 13:34
+ * @modified 2017-11-13 13:34
+ * @version 1.0.0
+ *
+ */
+@Component(
+    immediate = true, 
+    property = { 
+        "panel.app.order:Integer=800",
+        "panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT 
+    }, 
+    service = PanelApp.class
+)
+public class BibliographyManagerPanelApp extends BasePanelApp {
+
+    @Override
+    public String getPortletId() {
+        return PortletKeys.BIBLIOGRAPHY_MANAGER;
+    }
+
+    @Override
+    @Reference(target = "(javax.portlet.name=" + PortletKeys.BIBLIOGRAPHY_MANAGER + ")", unbind = "-")
+    public void setPortlet(Portlet portlet) {
+        super.setPortlet(portlet);
+    }
+
+}
